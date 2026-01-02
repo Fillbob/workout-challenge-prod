d835883dbda569a65e9169de31e3eaf3fef857df
Fillbob <romanwdziennik@gmail.com>
2026-01-02 12:16:58 -0500

    Make dashboard page dynamic

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { AnnouncementMarkdown } from "@/components/announcement-markdown";
 import { Announcement, listAnnouncements } from "@/lib/announcements";
 import { useRequireUser } from "@/lib/auth";
